f80bc0d7d9c6ffe922f7717bd3ff84fb5d79cf38
Ian Armstrong <ian.k.armstrong@gmail.com>
2022-05-17 11:11:59 +0200

    upate

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'aMain.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Geloofsbelydenis',
       theme: ThemeData(
-        primaryColor: Color.fromRGBO(58, 66, 86, 1.0),
+        primaryColor: const Color.fromRGBO(58, 66, 86, 1.0),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AMain(),
+      home: const AMain(),
     );
   }
 }
