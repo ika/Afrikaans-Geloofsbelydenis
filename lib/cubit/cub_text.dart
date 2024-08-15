881bff7c23f2d8e0e43ab9c0c4c54ddf10d46161
Ian K Armstrong <ian.k.armstrong@gmail.com>
2024-08-15 17:48:13 +0200

    update

---
+++ lib/cubit/cub_text.dart
@@ -0,0 +1,10 @@
+// import 'package:confesion_de_fe_de_westminster/utils/globals.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// class TextSizeCubit extends Cubit<double> {
+//   TextSizeCubit() : super(Globals.initialTextSize);
+
+//   void setSize(double state) => emit(state);
+//   void getSize() async => emit(Globals.initialTextSize);
+  
+// }
